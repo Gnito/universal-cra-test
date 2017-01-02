74ebf23805c214cf32f561c7f85bbc1c5ed62d6a
Vesa Luusua <vesa.luusua@gmail.com>
2017-01-02 12:24:15 +0200

    Express returns server side rendered response.

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+const React = require('react');
+const { renderToString } = require('react-dom/server');
+const { ServerRouter, createServerRenderContext } = require('react-router');
+
 const BUILD_DIR = path.join(__dirname, '..', 'build');
 const appPath = path.join(BUILD_DIR, 'server-bundle');
 
@@ -16,8 +20,50 @@ const expressApp = express();
 
 expressApp.use('/static', express.static(STATIC_DIR));
 
-app.get('*', (req, res) => {
-  res.send(indexHtml);
+
+expressApp.get('*', (req, res) => {
+
+  // first create a context for <ServerRouter>, it's where we keep the
+  // results of rendering for the second pass if necessary
+  const context = createServerRenderContext();
+
+  // render the first time
+  let ReactApp = renderToString(React.createElement(
+    ServerRouter,
+    { location: req.url, context: context },
+    React.createElement(App, {})
+  ));
+
+  // get the result
+  const result = context.getResult();
+
+  // the result will tell you if it redirected, if so, we ignore
+  // the ReactApp and send a proper redirect.
+  if (result.redirect) {
+    res.writeHead(301, {
+      Location: result.redirect.pathname
+    });
+    res.end();
+  } else {
+
+    // the result will tell you if there were any misses, if so
+    // we can send a 404 and then do a second render pass with
+    // the context to clue the <Miss> components into rendering
+    // this time (on the client they know from componentDidMount)
+    if (result.missed) {
+      res.writeHead(404);
+      ReactApp = renderToString(React.createElement(
+        ServerRouter,
+        { location: req.url, context: context },
+        React.createElement(App, {})
+      ));
+    }
+
+    // TODO quick test with replace flag
+    const markup = indexHtml.replace('{{SSR}}', ReactApp);
+    res.write(markup);
+    res.end();
+  }
 });
 
 
