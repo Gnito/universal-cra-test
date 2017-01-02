c17dfbd29a559acb7635162fbea6737a7f8f76bb
Vesa Luusua <vesa.luusua@gmail.com>
2017-01-02 12:17:20 +0200

    Generate server-bundle and initial server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
+const BUILD_DIR = path.join(__dirname, '..', 'build');
+const appPath = path.join(BUILD_DIR, 'server-bundle');
+
+const App = require(appPath).App.default;
+
+const STATIC_DIR = path.join(BUILD_DIR, 'static');
 const PORT = 4000;
-const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello, world');
+const indexHtml = fs.readFileSync(path.join(BUILD_DIR, 'index.html'), 'utf-8');
+
+const expressApp = express();
+
+expressApp.use('/static', express.static(STATIC_DIR));
+
+app.get('*', (req, res) => {
+  res.send(indexHtml);
 });
 
-app.listen(PORT, () => {
+
+expressApp.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
